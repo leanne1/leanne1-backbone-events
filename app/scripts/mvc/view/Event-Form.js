e211c51bf0e803b7b0798a32828e5dc763ea451c
leanne <leanne.tite@gmail.com>
2015-01-21 19:44:25 +0000

    wiring in new event form and validation

--- app/scripts/mvc/view/Event-Form.js
+++ app/scripts/mvc/view/Event-Form.js
@@ -4,18 +4,58 @@ define([
 	'jquery'], function(_, Backbone, $){
 	
 	var EventForm = Backbone.View.extend({
-			
+		events: {
+			'click [data-event-config="submit"]' : 'createEvent'
+		},
+
 		initialize: function(){
-			console.log('EventForm view initialized');
+			this.$controls = $('[data-event-property]');
+
+			//LISTEN FOR click on submit
+			//loop over form and for each field:
+			//	1. validate / show error message
+			//	2. get data
+			//use date to build up a new model
+			// pass the new model to an event card view and initialise it
+		},
+
+		createEvent: function(e){
+			e.preventDefault();
+			var self = this, eventData = {}, isValid, isClean = [], canSubmit;
+			this.$controls.each(function( i, control ){
+				$control = $(control);
+				if ($control.attr('required')) {
+					isValid = self.validate($control);
+					if (isValid) {
+						eventData[$control.data('event-propety')] = $control.val();
+					} 
+					isClean.push(isValid);
+				} else {
+					eventData[$control.data('event-propety')] = $control.val();
+				};
+			});
+			
+			canSubmit = _.every(isClean, function(control){
+				return (control === true);
+			});
+			canSubmit && this.createEventCard(eventData);
+		},
+
+		validate: function ($control) {
+			var isValid = $control.val() !== '';
+			if (!isValid) {
+				this.showErrorMEssage($control);	
+			} 
+			return isValid;
+		},
+
+		showErrorMEssage: function(){
+			console.log('form not valid message');
+		},
+
+		createEventCard: function(){
+			console.log('All clear - ready to create new event card!');
 		}	
-		// events: {
-		// 	'' : ''
-		// },
-		
-		// initialize: function(){
-		// 	console.log('Logging EventForm view on init: ', this);
-		// }
-	
 	});
 	
 	return EventForm;
